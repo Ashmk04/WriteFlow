5f7f37b7f995ef3d50e5be694dc9074c6fce4809
MUtkule Ashish <mutkuleashish1@gmail.com>
2026-07-03 18:37:54 +0530

    created CRUD operations

---
+++ src/appWrite/config.js
@@ -0,0 +1,96 @@
+import conf from "../conf/conf";
+import {ID, Databases, Client, Query, Storage, Permission, Role} from "appwrite"
+
+export class Service{
+
+    client = new Client()
+    databases
+    storage
+
+    constructor(){
+        this.client.setEndpoint(conf.appWriteURL)
+        this.client.setProject(conf.appWriteProjectId);
+
+        this.databases = new Databases(this.client)
+        this.storage = new this.storage(this.client)
+    }
+
+    async createPost(title, slug, content, image, status, userId) {
+        try {
+            return await this.databases.createDocument(
+                conf.appWriteDatabaseId,
+                conf.appWriteTableId,
+                slug,
+                {
+                    title, 
+                    content, 
+                    image, 
+                    status, 
+                    userId
+                })
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async updatePost({title, slug, content, image, status}) {
+        try {
+            return await this.databases.updateDocument(
+                conf.appWriteDatabaseId,
+                conf.appWriteTableId,
+                slug,
+                {
+                    title,
+                    content, 
+                    image,
+                    status
+                }
+            )
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async deletePost(slug) {
+        try {
+            await this.databases.deleteDocument(
+                conf.appWriteDatabaseId,
+                conf.appWriteTableId,
+                slug
+            )
+            return true
+        } catch (error) {
+            throw error
+            return false
+        }
+    }
+
+    async getPost(slug) {
+        try {
+            return await this.databases.getDocument(
+                conf.appWriteDatabaseId,
+                conf.appWriteTableId,
+                slug
+            )
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async getPosts(queries = [Query.equal("status", "active")]){
+        try {
+            return await this.databases.listDocuments(
+                conf.appWriteDatabaseId,
+                conf.appWriteTableId,
+                queries
+            )
+        } catch (error) {
+            throw error
+        }
+    } 
+
+}
+
+const service = new Service()
+
+export default service
