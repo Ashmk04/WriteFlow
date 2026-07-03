3d67035553f778a0a5936787b818be2a6c3f5f2b
MUtkule Ashish <mutkuleashish1@gmail.com>
2026-07-03 19:01:11 +0530

    created storage services

---
+++ src/appWrite/storage.js
@@ -0,0 +1,51 @@
+import conf from "../conf/conf";
+import {ID, Databases, Client, Query, Storage, Permission, Role} from "appwrite"
+
+export class StorageServices{
+    client = new Client()
+    storage
+
+    constructor() {
+        this.client.setEndpoint(conf.appWriteURL)
+        this.client.setProject(conf.appWriteProjectId)
+
+        this.storage = new Storage(this.client)
+    }
+
+    async uploadFile(file) {
+        try {
+            return await this.storage.createFile(
+                conf.appWriteBucketId,
+                ID.unique(),
+                file
+            )
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async deleteFile(fileId) {
+        try {
+            await this.storage.deleteFile(
+                conf.appWriteBucketId,
+                fileId
+            )
+            return true
+        } catch (error) {
+            throw error
+            return false
+        }
+    }
+
+    getFilePreview(fileId) {
+        return this.storage.getFilePreview(
+            conf.appWriteBucketId,
+            fileId
+        )      
+    }
+
+}
+
+const storageServices = new StorageServices() 
+
+export default storageServices
