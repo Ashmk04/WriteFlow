6b3051e2d16173b5dee3b56f3f34bf6d9823dcdc
MUtkule Ashish <mutkuleashish1@gmail.com>
2026-07-07 20:16:24 +0530

    configure some files

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,42 @@
-
+import React from "react"
+import { useState, useEffect } from "react"
+import { Header, Footer } from "./component/index"
+import {useDispatch} from "react-redux"
+import authService from "./appWrite/auth"
+import {login, logout} from "./store/authSlice"
+import { Outlet } from 'react-router-dom'
 
 function App() {
- 
-  console.log(import.meta.env.VITE_APPWRITE_BUCKET_ID)
 
-  return (
-    <>
-    
-    </>
-  )
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(()=>{
+    authService.currentUser()
+    .then((userData) => {
+      if(userData) {
+        dispatch(login({userData}))
+      }
+      else{
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false) )
+  }, [])
+  
+  return !loading ? 
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header/>
+        <main>
+          <Outlet/>
+        </main>
+        <Footer/>
+      </div>
+
+    </div>
+  : null
+
 }
 
 export default App
