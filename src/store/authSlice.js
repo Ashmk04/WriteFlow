c8ba95656fee90d853014758085e940fada98768
MUtkule Ashish <mutkuleashish1@gmail.com>
2026-07-07 20:17:22 +0530

    fixed authSlice

--- src/store/authSlice.js
+++ src/store/authSlice.js
@@ -1,17 +1,17 @@
 import {createSlice} from "@reduxjs/toolkit"
 
 const initialState = {
-    status = false,
-    userData = null
+    status: false,
+    userData: null
 }
 
 const authSlice = createSlice({
-    name: authService,
+    name: "auth",
     initialState,
     reducers: {
         login: (state, action)=>{
             state.status = true,
-            state.userData = action.payload
+            state.userData = action.payload.userData
         },
         logout: (state)=>{
             state.status = false,
