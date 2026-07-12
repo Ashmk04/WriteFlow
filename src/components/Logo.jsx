8e67568639ef09cbc5464f47675d82c9735d3177
MUtkule Ashish <mutkuleashish1@gmail.com>
2026-07-12 18:50:00 +0530

    style: update WriteFlow branding and UI

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React from "react";
+import logo from "../assets/WriteFlowLogo.png";
 
-export default function Logo({width = '100px'}) {
-  return (
-    <div>Logo</div>
-  )
+export default function Logo() {
+    return (
+        <div
+            
+            className="w-40 h-25 overflow-hidden flex items-center justify-center"
+        >
+            <img
+                src={logo}
+                alt="WriteFlow Logo"
+                className="w-full scale-[1.10] object-contain"
+            />
+        </div>
+    );
 }
-
