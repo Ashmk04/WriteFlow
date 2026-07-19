8d839137b9af8edc5ca2b0fe918e7ab2da312c67
MUtkule Ashish <mutkuleashish1@gmail.com>
2026-07-19 19:11:45 +0530

    recorrected service import

--- src/appWrite/auth.js
+++ src/appWrite/auth.js
@@ -1,6 +1,6 @@
 import { cache } from "react";
 import conf from "../conf/conf";
-import {Account, Client, ID} from "appWrite"
+import {Account, Client, ID} from "appwrite"
 
 export class AuthService{
 
